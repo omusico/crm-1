2ae234d78f4998893e820d4d3e6c07e5adc28a7d
nicklaw5 <nick_law@tpg.com.au>
2015-02-09 10:15:20 +1100

    more changes

--- app/libraries/HtmlBuilder.php
+++ app/libraries/HtmlBuilder.php
@@ -61,7 +61,7 @@ class HtmlBuilder {
 		                <td>'.date("d/m/Y", strtotime($invoice['due_date'])).'</td>
 		                <td>'.'$' . number_format($invoice['total'], 2, '.',',').'</td>
 		                <td>'.'$' . number_format($invoice['balance'], 2, '.',',').'</td>
-		                <td>'.ucfirst($invoice['status']).'</td>
+		                <td>'.ucwords(str_replace('_', ' ', $invoice['status'])).'</td>
 		                <td>
 		                    <a href="{{ url("/invoices/'.$invoice['invoice_id'].'") }}" class="btn btn-sm btn-info edit-invoice-link" data-invoice-id="" data-toggle="modal" data-target="#updateinvoiceModal">Edit</a>
 		                    <a href class="btn btn-sm btn-danger edit-invoice-link" data-invoice-id="" data-toggle="modal">PDF</a>
